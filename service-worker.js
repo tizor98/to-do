b4a69fa0c8848e9f2870b452e6638ea6000041e1
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2023-03-07 19:18:27 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/to-do/precache-manifest.0292f512133128113705f218f3b4c37a.js"
+  "todo.alberto.sofware/precache-manifest.69c647a56f19af17c304de8c3ce0faf4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/to-do/index.html", {
+workbox.routing.registerNavigationRoute("todo.alberto.sofware/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
