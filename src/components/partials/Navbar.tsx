303c95c49c67ce6b5243e5ed8f50596e4feb950a
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2023-03-14 00:09:18 -0500

    mobile view fixed on mobile and local storage added

--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -4,22 +4,11 @@ import logo from '../../assets/img/logo.png'
 export default function Navbar() {
    return (
       <header>
-         <nav className="navbar navbar-expand-lg navbar-light bg-black">
+         <nav className="navbar navbar-expand navbar-light bg-black">
             <div className="container-fluid">
-               <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-mdb-toggle="collapse"
-                  data-mdb-target="#navbarExample01"
-                  aria-controls="navbarExample01"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-               >
-                  <i className="fas fa-bars"></i>
-               </button>
-               <div className="collapse navbar-collapse" id="navbarExample01">
-               <img src={logo} alt="logo" height={30} />
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+               <div className="navbar-collapse">
+                  <img src={logo} alt="logo" height={30} />
+                  <ul className="navbar-nav me-auto mb-lg-0">
                      <li className="nav-item active">
                         <a className="nav-link text-white" aria-current="page" href="/">Home</a>
                      </li>
